20a08d704f822037412ae8bacc0d66a29f076dd9
rutil <rutil@DESKTOP-OK6RPPT>
2018-05-26 23:18:52 -0500

    Falta corregir Jframe

---
+++ ASESORIAS/src/POLIMORFISMO/Perro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POLIMORFISMO;
+
+/**
+ *
+ * @author rutil
+ */
+public class Perro {
+
+    public Perro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+ public String obtenerRuido() {
+        return "wao";
+    }
+    
+    public String toString() {
+        return "wao";
+    }    
+}
---
+++ ASESORIAS/src/POLIMORFISMO/Raton.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POLIMORFISMO;
+
+/**
+ *
+ * @author rutil
+ */
+public class Raton {
+
+    public Raton() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+ public String obtenerRuido() {
+        return "ii";
+    }
+    
+    public String toString() {
+        return "iii";
+    }    
+}
