a6c9d239472aa0131218f9316726df80ce073218
rutil <rutil@DESKTOP-OK6RPPT>
2018-06-01 14:03:38 -0500

    falta terminar u.u

---
+++ ASESORIAS/src/IMC/Aplicacion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IMC;
+
+/**
+ *
+ * @author rutil
+ */
+import javax.swing.JFrame;
+public class Aplicacion {
+public static void main(String[] args) {
+float peso;
+float altura;
+
+
+}
+
+    
+    }
+    
+
---
+++ ASESORIAS/src/IMC/imc.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IMC;
+
+/**
+ *
+ * @author rutil
+ */
+public class imc {
+
+private float peso;
+private float altura;
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+   
+
+      }
+
+    
+   
