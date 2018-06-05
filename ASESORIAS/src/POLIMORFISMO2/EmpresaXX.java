9ff59369dec371e1a78b1fa3f2a72e6539bd8ddb
T-101 <T-101@PC223>
2018-06-04 19:35:47 -0500

    Poli y Array =)

---
+++ ASESORIAS/src/POLIMORFISMO2/EmpresaXX.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POLIMORFISMO2;
+
+/**
+ *
+ * @author T-101
+ */
+import java.util.*;
+
+public class EmpresaXX {
+public static void main(String[] args) {
+ArrayList<Trabajador> tabajadores=new ArrayList<>();   
+
+    for (Trabajador tabajadore : tabajadores) {
+        
+    }
+    
+
+
+}
+   
+}
