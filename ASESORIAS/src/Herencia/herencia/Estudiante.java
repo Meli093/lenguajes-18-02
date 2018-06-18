cc1153423764fe32a88f74d2d23642421fe5bab0
my <my@my-Inspiron-5567>
2018-06-17 19:56:34 -0500

    Error en el Main

---
+++ ASESORIAS/src/Herencia/herencia/Estudiante.java
@@ -0,0 +1,23 @@
+package Herencia.herencia;
+
+public class Estudiante extends Persona{
+    //A hora llamos nuestro construcor.
+private int CodigoEstudiante;
+private float notaFinal;
+
+    public Estudiante(String nombre, String apellido, int edad,int CodigoEstudiante,float notaFinal ) {
+        super(nombre, apellido, edad);
+        this.CodigoEstudiante=CodigoEstudiante;
+        this.notaFinal=notaFinal;
+    }
+
+   
+
+      public void mostrarDatos(){
+        System.out.println("Nombre :"+getNombre()+"\n apellido :"+getApellido()+
+                "\n edad:"+getEdad()+"\n codigo Estudiante"+CodigoEstudiante+"\n Nota final"+notaFinal);
+            
+    }}
+
+
+
